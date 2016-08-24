4f2b5c6a852df02cb2e9347ada2dd28463b0e21b
Benjamin Israelson <benjamin.israelson@gmail.com>
2016-08-24 11:55:55 -0700

    add query param

--- src/main/java/com/disney/play/Application.java
+++ src/main/java/com/disney/play/Application.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.firebase.FirebaseApp;
@@ -18,10 +19,10 @@ import com.google.firebase.database.FirebaseDatabase;
 public class Application
 {
     @RequestMapping("/")
-    public String index()
+    public String index(@RequestParam(value="key") String key)
     {
         DatabaseReference d = FirebaseDatabase.getInstance().getReference();
-        d.child("azureTest").setValue("helloWorld-" + System.currentTimeMillis());
+        d.child("azureTest").setValue("helloWorld-key: [" + key + "]");
         return "{\"id\":1,\"content\":\"Hello, World!\"}";
     }
 

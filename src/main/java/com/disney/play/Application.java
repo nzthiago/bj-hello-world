754e5adc4df2040c67687deeead2b872ed126d05
Benjamin Israelson <benjamin.israelson@gmail.com>
2016-08-25 12:02:53 -0700

    use redis pool

--- src/main/java/com/disney/play/Application.java
+++ src/main/java/com/disney/play/Application.java
@@ -2,6 +2,8 @@ package com.disney.play;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,12 +17,28 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
 
 @SpringBootApplication
 @RestController
 public class Application
 {
+    final static ShardedJedisPool redisStatsPool;
+    
+    static
+    {
+        List<JedisShardInfo> redisClickShard = new ArrayList<JedisShardInfo>();
+        JedisShardInfo shardInfo = new JedisShardInfo("bj-hello-world.redis.cache.windows.net", 6379);
+        shardInfo.setPassword("FpaDShz8Uj6frCAduCJuhQSlyPrz7//nCu1GD14+enU="); /* Use your access key. */
+        redisClickShard.add(shardInfo);
+        
+        JedisPoolConfig config = new JedisPoolConfig();
+        redisStatsPool = new ShardedJedisPool( config, redisClickShard);
+    }
+    
     @RequestMapping("/")
     public String index(@RequestParam(value="key", defaultValue = "no key") String key)
     {
@@ -28,26 +46,19 @@ public class Application
         d.child("azureTest").setValue("helloWorld-key: [" + key + "]");
         
         /* In this line, replace <name> with your cache name: */
-        JedisShardInfo shardInfo = new JedisShardInfo("bj-hello-world.redis.cache.windows.net", 6379);
-        shardInfo.setPassword("FpaDShz8Uj6frCAduCJuhQSlyPrz7//nCu1GD14+enU="); /* Use your access key. */
+//        JedisShardInfo shardInfo = new JedisShardInfo("bj-hello-world.redis.cache.windows.net", 6379);
+//        shardInfo.setPassword("FpaDShz8Uj6frCAduCJuhQSlyPrz7//nCu1GD14+enU="); /* Use your access key. */
 
-        Jedis jedis = new Jedis(shardInfo);
+//        Jedis jedis = new Jedis(shardInfo);
         
         String value = "NOT-SET";
         
-        try
-        {
+        ShardedJedis shardedJedis = redisStatsPool.getResource();
+        final Jedis jedis = shardedJedis.getShard("") ;
+
             jedis.set("key", key);
             value = jedis.get("key");
-        }
-        catch(Exception e)
-        {
-            value = e.toString();
-        }
-        finally 
-        {
-            jedis.close();
-        }
+    
         
         return "{\"redis\":" + value + ",\"content\":\"Hello, World! at: " + System.currentTimeMillis() + "\"}";
     }
